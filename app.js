f3f6b5e31a0d7b51934e8ea3dfdd5be05020845f
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-03-31 22:51:33 +0500

    Add Question 06.

--- app.js
+++ app.js
@@ -24,4 +24,71 @@ for (i=1; i<=takeNum; i++){
 // Question # 05
 var fruits = ['apple' , 'banana' , 'mango', 'orange', 'strawberry']
 
-document.write(`${fruits} <br> Element at index 0 is ${fruits[0]} <br> Element at index 1 is ${fruits[1]} <br> Element at index 2 is ${fruits[2]} <br> Element at index 3 is ${fruits[3]} <br> Element at index 4 is ${fruits[4]}`)
+document.write(`${fruits} <br> Element at index 0 is ${fruits[0]} <br> Element at index 1 is ${fruits[1]} <br> Element at index 2 is ${fruits[2]} <br> Element at index 3 is ${fruits[3]} <br> Element at index 4 is ${fruits[4]} <br>`)
+
+//Question 06
+document.write("<h2>Counting</h2>")
+for (var i=1; i<=15; i++){
+    document.write(i + "<br>")
+}
+
+document.write("<h2>Reverse Counting</h2>")
+
+for (var i=10; i>=1; i--){
+    document.write(i + "<br>")
+}
+
+document.write("<h2>Even Number</h2>")
+
+for (let i = 0; i <= 20; i+= 2){
+    document.write(i + "<br>")
+}
+
+document.write("<h2>Odd Number</h2>")
+
+for (var i = 0; i <= 20; i++) {
+    if (i % 2 != 0) {
+      document.write(i + "</br>")
+    }
+  }
+
+  document.write("<h2>Series Number</h2>")
+
+  for (let i = 2; i <= 20; i+= 2){
+      document.write(i + "k" + "<br>")
+  }
+
+  //Question # 07
+//   var bakery = ['cake', 'apple pie', 'cookie', 'chips', 'patties']
+//   var userInput= prompt("Enter Bakery Item")
+
+  //Question # 08
+// document.write("<h2>Find Largest Number</h2>")
+
+// var arr = [3, 6, 2, 56, 32, 5, 89, 32, 212];
+// var largest = arr[0];
+
+// for (var i = 0; i < arr.length; i++) {
+//   if (arr[i] > largest ) {
+//     largest = arr[i];
+//   }
+// }
+// document.write(`Array Items ${arr} <br> The Largest Number is ${largest}`);
+
+
+//Question # 09
+// document.write("<h2>Find Lowest Number</h2>")
+// var arr = [3, 6, 2, 56, 32, 5, 89, 32, 212];
+// var smallest= arr[0];
+
+// for (var i= 0; i>arr.length; i++){
+//     if (arr[i] < smallest){
+//         smallest = arr[i];
+//     }
+// }
+// document.write(`Array Items ${arr} <br> The Smallest Number is ${smallest}`)
+
+// Question # 10
+
+
+  
