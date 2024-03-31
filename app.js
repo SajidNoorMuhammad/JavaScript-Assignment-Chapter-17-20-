21af915f7962edc0b212efbeaa4921890e9da24c
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-03-31 18:38:06 +0500

    Add Question # 02.

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 // Question # 01
-var Arr= [[hello][Hi][Bye]];
+// var Arr= [[hello][Hi][Bye]];
 
 // Question # 02
+var arrNum= ['0'+123]
+var arrNum1= [1012]
+var arrNum3= [2101]
+var arr= [arrNum.join(), arrNum1.join(), arrNum3.join()]
+document.write(`<h1>${arrNum} <br> ${arrNum1} <br> ${arrNum3}</h1>`)
+
